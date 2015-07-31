d3b99dd62c850a9699eeece72fcd4379e204ca38
JUNGMINKIM <ministar0259@gmail.com>
2015-07-31 13:23:37 +0900

    Create Palindrome.rb

---
+++ Palindrome.rb
@@ -0,0 +1,15 @@
+def Palindrome(str)
+
+  chars = str.split(" ").join("").chars.to_a
+  chars.each_index do |x|
+    if(chars[x] != chars[chars.length - x - 1])
+      return "false"
+    end
+  end
+  return "true"
+         
+end
+
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+Palindrome(STDIN.gets)           
